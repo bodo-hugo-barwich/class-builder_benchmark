3e53647496cc102e3b3090448def87b8564a1bc1
Bodo (Hugo) Barwich <b.barwich@hotmail.com>
2025-12-29 14:59:51 +0300

    Mojo::Base test class

--- lib/Foo.pm
+++ lib/Foo.pm
@@ -1,13 +1,14 @@
 use v5.40;
 
 package Foo {
-    use Benchmark qw(cmpthese);
+    use Benchmark   qw(cmpthese);
     use Devel::Size qw(total_size);
 
     use Foo::FeatureClass;
     use Foo::ObjectPad;
     use Foo::ClassAccessorLite;
     use Foo::Mouse;
+    use Foo::Mojo;
     use Foo::Moose;
     use Foo::MooseXS;
     use Foo::Moo;
@@ -18,16 +19,17 @@ package Foo {
     use Foo::BlessArray;
 
     my $TITLE_MAPPING = {
-        'Foo::FeatureClass'      => sprintf('`class feature (perl: %s)`', $]),
-        'Foo::ObjectPad'         => sprintf('`Object::Pad@%s`', Object::Pad->VERSION),
-        'Foo::ClassAccessorLite' => sprintf('`Class::Accessor::Lite@%s`', Class::Accessor::Lite->VERSION),
-        'Foo::Mouse'             => sprintf('`Mouse@%s`', Mouse->VERSION),
-        'Foo::Moo'               => sprintf('`Moo@%s`', Moo->VERSION),
-        'Foo::MooXS'             => sprintf('`Moo@%s (XSConstructor + XSAccessor)`', Moo->VERSION),
-        'Foo::Moose'             => sprintf('`Moose@%s`', Moose->VERSION),
-        'Foo::MooseXS'           => sprintf('`Moose@%s (XSAccessor)`', Moose->VERSION),
-        'Foo::ClassTiny'         => sprintf('`Class::Tiny@%s`', Class::Tiny->VERSION),
-        'Foo::ObjectTiny'        => sprintf('`Object::Tiny@%s`', Object::Tiny->VERSION),
+        'Foo::FeatureClass'      => sprintf( '`class feature (perl: %s)`',            $] ),
+        'Foo::ObjectPad'         => sprintf( '`Object::Pad@%s`',                      Object::Pad->VERSION ),
+        'Foo::ClassAccessorLite' => sprintf( '`Class::Accessor::Lite@%s`',            Class::Accessor::Lite->VERSION ),
+        'Foo::Mouse'             => sprintf( '`Mouse@%s`',                            Mouse->VERSION ),
+        'Foo::Mojo'              => sprintf( '`Mojo::Base@%s`',                       $Mojolicious::VERSION ),
+        'Foo::Moo'               => sprintf( '`Moo@%s`',                              Moo->VERSION ),
+        'Foo::MooXS'             => sprintf( '`Moo@%s (XSConstructor + XSAccessor)`', Moo->VERSION ),
+        'Foo::Moose'             => sprintf( '`Moose@%s`',                            Moose->VERSION ),
+        'Foo::MooseXS'           => sprintf( '`Moose@%s (XSAccessor)`',               Moose->VERSION ),
+        'Foo::ClassTiny'         => sprintf( '`Class::Tiny@%s`',                      Class::Tiny->VERSION ),
+        'Foo::ObjectTiny'        => sprintf( '`Object::Tiny@%s`',                     Object::Tiny->VERSION ),
         'Foo::Bless'             => '`bless hashref`',
         'Foo::BlessArray'        => '`bless arrayref`',
     };
@@ -35,25 +37,29 @@ package Foo {
     sub run_benchmark($create_main_logic_coderef) {
         my $title_mapping = $TITLE_MAPPING;
 
-        my $rows = cmpthese(-1, {
-            map {
-                my $benchmark_class = $_;
-                my $title = $title_mapping->{$benchmark_class};
-                $title => $create_main_logic_coderef->($benchmark_class);
-            } keys %$title_mapping,
-        }, 'none');
+        my $rows = cmpthese(
+            -1,
+            {
+                map {
+                    my $benchmark_class = $_;
+                    my $title           = $title_mapping->{$benchmark_class};
+                    $title => $create_main_logic_coderef->($benchmark_class);
+                } keys %$title_mapping,
+            },
+            'none'
+        );
 
         my $header = shift @$rows;
 
         # find the index of the 'class feature' row
-        my ($x) = map { $header->[$_] =~ /class feature/ ? ($_) : ()  } 0 .. $#$header;
+        my ($x) =
+          map { $header->[$_] =~ /class feature/ ? ($_) : () } 0 .. $#$header;
 
         say join "\t", qw(Rate Compare Title);
         for my $row (@$rows) {
-            say join "\t",
-                $row->[1], # Rate
-                $row->[$x], # compare 'class feature'
-                $row->[0]; # Title
+            say join "\t", $row->[1],    # Rate
+              $row->[$x],                # compare 'class feature'
+              $row->[0];                 # Title
         }
     }
 
@@ -61,34 +67,36 @@ package Foo {
         my $title_mapping = $TITLE_MAPPING;
 
         my %result;
-        for my $benchmark_class (keys %$title_mapping) {
+        for my $benchmark_class ( keys %$title_mapping ) {
             my $title = $title_mapping->{$benchmark_class};
 
-            my $logic = $create_main_logic_coderef->($benchmark_class);
-            my $result = $logic->();
+            my $logic      = $create_main_logic_coderef->($benchmark_class);
+            my $result     = $logic->();
             my $total_size = total_size($result);
 
             $result{$title} = $total_size;
         }
 
-        my ($size_of_feature_class) = map { $_ =~ /class feature/ ? ($result{$_}) : ()  } keys %result;
+        my ($size_of_feature_class) =
+          map { $_ =~ /class feature/ ? ( $result{$_} ) : () } keys %result;
 
         my @report;
-        for my $title (sort { $result{$a} <=> $result{$b} } keys %result) {
-            my $total_size = $result{$title};
-            my $readable_size = sprintf("%.1f KB", $total_size / 1024);
+        for my $title ( sort { $result{$a} <=> $result{$b} } keys %result ) {
+            my $total_size    = $result{$title};
+            my $readable_size = sprintf( "%.1f KB", $total_size / 1024 );
 
-            my $size_rate = (($total_size - $size_of_feature_class) / $size_of_feature_class) * 100;
-            my $readable_size_rate = $size_rate == 0 ? '--'
-                                   : $size_rate <  0 ? sprintf('-%.1f%%', $size_rate)
-                                   :                   sprintf('%.1f%%', $size_rate);
+            my $size_rate = ( ( $total_size - $size_of_feature_class ) / $size_of_feature_class ) * 100;
+            my $readable_size_rate =
+                $size_rate == 0 ? '--'
+              : $size_rate < 0  ? sprintf( '-%.1f%%', $size_rate )
+              :                   sprintf( '%.1f%%', $size_rate );
 
             push @report => [ $readable_size, $readable_size_rate, $title ];
         }
 
-        say join("\t", 'Size', 'Compare', 'Title');
+        say join( "\t", 'Size', 'Compare', 'Title' );
         for my $report (@report) {
-            say join("\t", @$report);
+            say join( "\t", @$report );
         }
     }
 }
